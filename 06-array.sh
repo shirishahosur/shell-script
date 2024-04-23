dc289b1a25b715017395415ed6a8941967e37a9d
shirisha hosur <shirisha.h916@gmail.com>
2024-04-23 21:57:53 +0530

    some-message

---
+++ 06-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MOVIES=("RRR" "DJTILLU" "murari")
+
+# size of above array is 3
+# index are 0,1,2
+
+# list always start with 0
+
+
+echo "First Movie is: ${MOVIES[0]}"
+echo "First Movie is: ${MOVIES[1]}"
+echo "First Movie is: ${MOVIES[@]}"
