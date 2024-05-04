aecfee8ea8e0dd4e84e8b72749f0d40b471a4d84
shirisha hosur <shirisha.h916@gmail.com>
2024-05-04 21:52:34 +0530

    some-message

---
+++ 16-current-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "Before calling other script, course: $COURSE"
+echo "Process ID of current shell script: $$"
+
+
+echo "After calling other script, course: $COURSE"
